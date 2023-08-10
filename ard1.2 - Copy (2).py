d5d2bac29ce9dffc9fc370cb4bd69e52cdf4a5f4
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-08-10 15:06:36 +0530

    Update ard_py_1.2_.py

--- ard1.2 - Copy (2).py
+++ ard1.2 - Copy (2).py
@@ -1,63 +1,93 @@
-import openpyxl
-import winsound
-import smtplib, ssl
+import openpyx1
+import satplib, ssl
 import serial
-ser=serial.Serial("COM7",9600)
 
-rfidTag=["9A 55 02 B0", "15 28 90 5C","23 5B E8 11","D3 37 F5 5C","A3 AA F1 0B"]
-rfidName=["RFID TAG BLUE 1111", "M.VARUN REDDY", "WHITE CARD1111","RAYAN SINGH","RFID TAG BLUE 2222"]
-rfidEmail=["espArduno@gmail.com","varunmr2003@gmail.com","espArduno@gmail.com","rayan.singh2021@vitstudent.ac.in","rayan.singh2021@vitstudent.ac.in"]
+ser=serial.Serial("COM7",9608)
+rfidTag=["9A 55 82 Be", "15 28 90 5C","AS EF 91 38","71 98 DA DF"]
+rfidName=["TEST", "M.VARUN REDDY", "SAINATH CHAKARE","VARUN SINGH"] 
+rfidEmail=["espArduno@gmail.com", "varunmr2003@gmail.com", "sainath.chakare2821@ vitstudent.ac.in","varunsingh2021@vitstudent.ac.in"]
+
+path="test.xlsx"
+wbObj=openpyx1.load_workbook(path)
+sheetObj=wbObj.active
 
 
-def isAuth(UID):        #function that returns index of the uid 
-    for i in rfidTag:
-        if i==UID:
-            x=rfidTag.index(i)
+def isAuth (UID): #function that returns index of the uid
+    for i in rfidTag: 
+        if i == UID:
+            x=rfidTag.index(1) 
             return x
     return -1
 
-        
 ## EMAIL CONNECTION
-port = 465  # For SSL
+port = 465 # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "esparduno@gmail.com"  # Sender email
+sender_email ="esparduno@gmail.com" # Sender email
 password = "rsywlixcqxblfyhw"
 
+# SETTING A NEW DATE
+def newDate():
+    dateStr=input("Enter the date for attendance: colm-sheetObj.max_column #maximum no of columns")# input date 
+    colm=sheetObj.max_column
+    sheetObj.cell(row=1, column=colm+1) 
+    cellObj.value=dateStr
+    for x in range(2, len(rfidTag)+2):
+        cellObj=sheetObj.cell(row=x, column=colm+1)
+        cellObj.value="absent"
+    wbObj.save(filename="test.xlsx")
+    return dateStr
 
-modeInput=input("""
+
+def markPresent(index): 
+    colm=sheetObj.max_column
+    cell0bj=sheetObj.cell(row=index+2, column=colm) 
+    cell0bj.value="present"
+    wbObj.save(filename="test.xlsx")
+
+def addewEntry(index,name):
+    colm=sheetObj.max_column
+    roww=sheetObj.max_row
+    cellObj=sheetObj.cell(row=roww+1,column=1)
+    cellObj.value=name
+    wbObj.save(filename="test.xlxs")
+
+modeInput = input("""
 HELLO WORLD!, TO THE RFID ATTENDANCE SYSTEM
 
+COMMANDS:
+
 1. TO TAKE ATTENDANCE
-2. TO QUIT PROGRAM
+2. TO QUTI THE PROGRAM
+
+YOUR INPUT:
 """)
 
 if int(modeInput)==1:
+    #DATE SECTION:
+    dateStr2 = newDate()
     while True:
-        index=-1
-        line1 = ser.readline().decode().rstrip() #approximate line
+        index = -1
+        # line1 = ser.readline().decode().rstrip() #approximate your rfid card to the sensor
         print("Approximate your card... ")
-        line1 = ser.readline().decode().strip() #uid tag line
-        print("UID : "+line1)
-        index = isAuth(line1) # authorisation func call
-
+        line1 = ser.readline().decode.strip() #uid tag line
+        print("UID: "+line1)
+        index = isAuth(line1) # authorisatiion func call
+        
         if index == -1:
-            winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS)
-            print("Access denied")
+            print("Access Denied")
             continue
-
         print("Hello, "+rfidName[index])
 
-        #EMAIL SECTION:
-        receiver_email = rfidEmail[index]  # Enter receiver address
-        message = "Marked attendance for "##+dateStr2
+        #excel data base updation
+        markPresent(index)
+
+        #Email Section
+        receiver_email = rfidEmail[index] # enter receiver address
+        message = "Marked attendance for "+dateStr2
 
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        with smptlib.SMTP_SSL(smtp_server, port, context = context) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message)
-
 else:
     exit()
-
-
-
